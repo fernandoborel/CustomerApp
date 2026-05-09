7c6506df7dba5e2a1d2d325f7a052b8788c21f74
fernandoborel-fiotec <fernandoborel@fiotec.fiocruz.br>
2026-05-08 21:47:12 -0300

    Serviço de autenticação de usuário.

--- CustomerApp.API/Controllers/UsersController.cs
+++ CustomerApp.API/Controllers/UsersController.cs
@@ -1,18 +1,63 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CustomerApp.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
 
-namespace CustomerApp.API.Controllers
+namespace CustomerApp.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsersController : ControllerBase
+    private readonly MongoDbService _mongoDbService;
+    private readonly PasswordService _passwordService;
+    private readonly JwtService _jwtService;
+
+    public UsersController(MongoDbService mongoDbService, PasswordService passwordService, JwtService jwtService)
     {
-        /// <summary>
-        /// ENDPOINT para autenticação dos usuários.
-        /// </summary>
-        [HttpPost("auth")]
-        public async Task<IActionResult> Auth()
+        _mongoDbService = mongoDbService;
+        _passwordService = passwordService;
+        _jwtService = jwtService;
+    }
+
+    /// <summary>
+    /// ENDPOINT para autenticação dos usuários.
+    /// </summary>
+    [HttpPost("auth")]
+    public async Task<IActionResult> Auth([FromBody] AuthRequest request)
+    {
+        var existingUser = await _mongoDbService.Users
+                            .Find(u => u.Email == request.email)
+                            .FirstOrDefaultAsync();
+
+        if (existingUser is null)
         {
-            return Ok();
+            return Unauthorized(new { message = "Credenciais inválidas." });
         }
+
+        var passwordOk = _passwordService.VerifyPasswords(request.password, existingUser.PasswordHash!);
+
+        if (!passwordOk)
+        {
+            return Unauthorized(new { message = "Acesso negado." });
+        }
+
+        var token = _jwtService.GenerateToken(existingUser);
+
+        return Ok(new
+        {
+            accessToken = token,
+            tokenType = "Bearer",
+            user = new
+            {
+                existingUser.Id,
+                existingUser.Name,
+                existingUser.Email
+            }
+        });
     }
 }
+
+public record AuthRequest(
+        string email,
+        string password
+    );
